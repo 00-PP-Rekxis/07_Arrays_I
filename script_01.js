06f4f1970dea8966c2b1834171ef91b72cc1dbe7
Rekxis <Rekxis@gmx.de>
2023-07-19 10:24:44 +0200

    03a. Funktionalität mit Array 1

--- script_01.js
+++ script_01.js
@@ -8,15 +8,15 @@
  */
 
 /*** 02. Theorie: Array */
-let arr;
+// let arr;
 // arr = new Array(); // Konstruktor
-arr = []; // Literal
-arr = [2, 7, 11, 4, 8, 10];
-output(arr);
-output(arr.length); // Anzahl der Elemente
-output(arr[0]);     // Index 0 (1. Pos. im Array)
-output(arr[4]);     // letze Pos. / konkret
-output(arr[arr.length-1]); // letze Pos. / allgemein
+// arr = []; // Literal
+// arr = [2, 7, 11, 4, 8, 10];
+// output(arr);
+// output(arr.length); // Anzahl der Elemente
+// output(arr[0]);     // Index 0 (1. Pos. im Array)
+// output(arr[4]);     // letze Pos. / konkret
+// output(arr[arr.length-1]); // letze Pos. / allgemein
 
 
 /**
@@ -29,30 +29,51 @@ output(arr[arr.length-1]); // letze Pos. / allgemein
 // --> "Ich bin Max Mütze."
 // output(getSentence("Ich","bin","Max","Mütze"));
 // output(getSentence("Ich","bin","die","coole","Maxine","Mützerich"));
-output(getSentence(["Ich","bin","Max","Mütze", "cool"]));
 
-// function getSentence(word1,word2,word3,word4,word5,word6) {
-//     const GAP = " ";
-//     const PUNCT = ".";
-//     let str =   word1 + GAP +
-//                 word2 + GAP +
-//                 word3 + GAP +
-//                 word4 + GAP +
-//                 word5 + GAP +
-//                 word6 +
-//                 PUNCT;
-//     return str;
-// }
 
-function getSentence(ary) {
+function getSentence(word1,word2,word3,word4,word5,word6) {
+    const GAP = " ";
+    const PUNCT = ".";
+    let str =   word1 + GAP +
+                word2 + GAP +
+                word3 + GAP +
+                word4 + GAP +
+                word5 + GAP +
+                word6 +
+                PUNCT;
+    return str;
+}
+
+/*** 01a. Funktionalität mit Array 1 */
+// Kritik | Überlegung
+// word1,word2, .... wordN :: Semantische Struktur
+// arr[0],arr[1] ... arr[n-1] :: Numerische Struktur (0,1,2,3 ... n)
+// --> Transformation semantisches Problem --> numerisches Problem
+
+// output(getSentenceArr(["Ich","bin","die","coole","Maxine","Mützerich"]));
+function getSentenceArr(arr) {
+    const GAP = " ";
+    const PUNCT = ".";
+    let str =   arr[0] + GAP +
+                arr[1] + GAP +
+                arr[2] + GAP +
+                arr[3] + GAP +
+                arr[4] + GAP +
+                arr[5] +
+                PUNCT;
+    return str;
+}
+
+// output(getSentenceShort(["Ich","bin","Max","Mütze", "cool"]));
+function getSentenceShort(ary) {
     const GAP = " ";
     const PUNCT = ".";
     let outputStr = "";
-    // for (let i = 0; i < ary.length; i++) {
-    //     outputStr += ary[i] + GAP;
-    // }
-    // return outputStr.trim() + PUNCT;
-    return outputStr = ary.join(' ') + PUNCT; // internal function | better
+    for (let i = 0; i < ary.length; i++) {
+        outputStr += ary[i] + GAP;
+    }
+    return outputStr.trim() + PUNCT;
+    // return ary.join(' ') + PUNCT; // internal function | better
 }
 
 
