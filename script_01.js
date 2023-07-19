74dd5c5f0fb30c71a17997da455a6a33ae99bc28
Rekxis <Rekxis@gmx.de>
2023-07-19 09:18:53 +0200

    first draft

--- script_01.js
+++ script_01.js
@@ -21,17 +21,31 @@
 /*** 01. Funktionalität mit Einzelparametern */
 // --> "Ich bin Max Mütze."
 // output(getSentence("Ich","bin","Max","Mütze"));
+// output(getSentence("Ich","bin","die","coole","Maxine","Mützerich"));
+output(getSentence(["Ich","bin","Max","Mütze", "cool"]));
 
-function getSentence(word1,word2,word3,word4) {
+// function getSentence(word1,word2,word3,word4,word5,word6) {
+//     const GAP = " ";
+//     const PUNCT = ".";
+//     let str =   word1 + GAP +
+//                 word2 + GAP +
+//                 word3 + GAP +
+//                 word4 + GAP +
+//                 word5 + GAP +
+//                 word6 +
+//                 PUNCT;
+//     return str;
+// }
+
+function getSentence(ary) {
     const GAP = " ";
     const PUNCT = ".";
-    let str =   word1 + GAP +
-                word2 + GAP +
-                word3 + GAP +
-                word4 + GAP +
-                PUNCT;
-
-    return str;
+    let outputStr = "";
+    for (let i = 0; i < ary.length; i++) {
+        outputStr += ary[i] + GAP;
+    }
+    outputStr = outputStr.trim() + PUNCT;
+    return outputStr
 }
 
 
@@ -42,5 +56,3 @@ function getSentence(word1,word2,word3,word4) {
 function output(outputStr) {
     console.log(outputStr);
 }
-
-
